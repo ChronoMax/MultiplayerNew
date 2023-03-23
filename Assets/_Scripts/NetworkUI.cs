1659db8ae1dc7a596c91fa162a1253fc6b9a343a
Max Struijs <maxstruys32@gmail.com>
2023-03-23 12:18:18 +0100

    Updated Files

--- Assets/_Scripts/NetworkUI.cs
+++ Assets/_Scripts/NetworkUI.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
-
 public class NetworkUI : MonoBehaviour
 {
     [SerializeField] private Button serverButton;
