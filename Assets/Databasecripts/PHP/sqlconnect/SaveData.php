48eea80bd7de8874c0f02f0bc9e4b80c6f6b5410
Max Struijs <maxstruys32@gmail.com>
2023-04-25 14:36:18 +0200

    Added the ability to add and save levels to a player

--- Assets/Databasecripts/PHP/sqlconnect/SaveData.php
+++ Assets/Databasecripts/PHP/sqlconnect/SaveData.php
@@ -14,21 +14,17 @@
 
     //Checking if the name already excists in the database.
     $username = $_POST ["username"];
-    $password = $_POST["password"];
+    $score = $_POST["score"];
 
     $namequerycheck = "SELECT username FROM players WHERE username='" . $username ."'; ";
     $namecheck = mysqli_query($conn, $namequerycheck) or die ("2: Name check failed.");
-
-    if(mysqli_num_rows($namecheck) = 0){
-        echo"3: Name already exist."; //if the name already exists, give back this code.
+        if(mysqli_num_rows($namecheck) != 1){
+        echo "5: username does not excist";
         exit();
     }
 
-    //Adding user to database
-    $salt = "\$5\$round=5000\$" . "steamedhams" . $username . "\$";
-    $hash = crypt($password, $salt);
+    $updatequery = "UPDATE players SET score = " . $score . " WHERE username = '" . $username . "';";
+    mysqli_query($conn, $updatequery) or die ("7 : Saving query failed");
 
-    $insertuserquery = "INSERT INTO players (username, hash, salt) VALUES ('" . $username . "', '" . $hash . "', '" . $salt . "'); ";
-
-    mysqli_query($conn, $insertuserquery) or die ("4: inserting player failed");
+    echo 0;
 ?>
