e5b148375f88cf110011cac1a9d201a7869bc76a
Max Struijs <maxstruys32@gmail.com>
2023-05-17 15:57:17 +0200

    Optimised the player script

--- Assets/_Scripts/PlayerNetwork.cs
+++ Assets/_Scripts/PlayerNetwork.cs
@@ -1,54 +1,44 @@
 using UnityEngine;
 using Unity.Netcode;
-using Unity.Collections;
 using TMPro;
-using UnityEngine.VFX;
-using UnityEngine.SceneManagement;
 
 public class PlayerNetwork : NetworkBehaviour
 {
     [Header("Health")]
     public NetworkVariable<int> health = new NetworkVariable<int>();
     public TMP_Text healthText, timerText;
-    public bool hit, dead = false;
+    public bool hit, dead;
 
-    [SerializeField] GameObject playerComponents, gameplayUI, menuUI;
-
-    private float moveSpeed = 5.0f;
-    private float lookSpeed = 2.0f;
-    private float verticalLookRotation = 0.0f;
-
+    [SerializeField] GameObject playerComponents, gameplayUI, menuUI, mainCam;
     [SerializeField] Transform cameraTransform;
-
-    [Header("Playername")]
-    [SerializeField] string playerName;
     [SerializeField] TextMeshProUGUI playerNameText;
-
     [SerializeField] float timeBetweenFire;
-    float firetimer;
 
     [Header("VFX/SFX")]
-    public AudioSource gunAudio;
-    public ParticleSystem gunshot, blood;
+    [SerializeField] ParticleSystem gunshot, blood;
+    [SerializeField] AudioSource shotshound;
+    
+    float verticalLookRotation = 0.0f;
+    float fireTimer;
+    float moveSpeed = 5.0f;
+    float lookSpeed = 2.0f;
+    string playerName;
 
-    public GameObject maincam;
-
-    public override void OnNetworkSpawn()
+    public override void OnNetworkSpawn() //when the player spawns and the player is the owner
     {
         if (IsOwner)
         {
             playerNameText.text = playerName;
             health.Value = 100;
-            maincam.SetActive(false);
+            mainCam.SetActive(false);
             playerComponents.SetActive(true);
-
             Screen.lockCursor = true;
         }
     }
 
     private void Update()
     {
-        if (!IsOwner && IsServer)
+        if (!IsOwner && IsServer) //the server can do the following code:
         {
             if (hit)
             {
@@ -58,27 +48,22 @@ public class PlayerNetwork : NetworkBehaviour
 
             if (dead)
             {
-                despawnclientrpc();
+                DespawnClientRpc();
                 dead = false;
             }
-
         }
 
-        if (!IsOwner) return;
-
-        //if (health.Value == 0)
-        //{
-        //    maincam.SetActive(true);
-        //    Screen.lockCursor = false;
-        //}
+        if (!IsOwner) return; //from here the code will only run for his owner
 
         healthText.text = "Health: " + health.Value.ToString();
-        timerText.text = "Reload: " + firetimer.ToString("f1");
-
-        if (Input.GetKeyDown(KeyCode.Escape))
+        timerText.text = "Reload: " + fireTimer.ToString("f1");
+        
+        /*
+         * HANDELING PAUSE MENU
+         */
+        if (Input.GetKeyDown(KeyCode.Escape)) //handeling of the pause menu
         {
             Screen.lockCursor = false;
-
             if (gameplayUI.activeInHierarchy)
             {
                 gameplayUI.SetActive(false);
@@ -95,20 +80,26 @@ public class PlayerNetwork : NetworkBehaviour
             Screen.lockCursor = true;
         }
 
-        if (Input.GetMouseButtonDown(0) && !menuUI.activeInHierarchy)
+        /*
+         * HANDELING SHOOTING
+         */
+        if (Input.GetMouseButtonDown(0) && !menuUI.activeInHierarchy) //if the user is active in game the player can shoot a bullet
         {
-            if (firetimer <= 0)
+            if (fireTimer <= 0)
             {
                 ShootBulletServerRpc(10, cameraTransform.position, cameraTransform.forward);
-                firetimer = timeBetweenFire;
+                fireTimer = timeBetweenFire;
             }
         }
 
-        if (firetimer > 0)
+        if (fireTimer > 0) //checks if the timer
         {
-            firetimer -= Time.deltaTime;
+            fireTimer -= Time.deltaTime;
         }
 
+        /*
+         * PLAYER MOVEMENT
+         */
         // Get user input
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
@@ -130,23 +121,66 @@ public class PlayerNetwork : NetworkBehaviour
         }
     }
 
-    public void Respawn()
+    public void Respawn() //respawns the player (local) and sets the position
     {
-        maincam.SetActive(false);
-        gameObject.transform.position = new Vector3(0, 0, 0);
+        mainCam.SetActive(false);
+        gameObject.transform.position = Vector3.zero;
         playerComponents.SetActive(true);
-        respawnServerRpc(100);
-        //RespawnClientRpc();
+
+        RespawnServerRpc(100);
+    }
+
+    //All the messages from the server to the clients
+    #region ClientRPC
+    [ClientRpc]
+    public void BloodVFXClientRpc() //plays the blood animation for every player
+    {
+        blood.Play();
+    }
+
+    [ClientRpc]
+    public void ShotClientRpc() //plays the gunshot sound and VFX for every player
+    {
+        gunshot.Play();
+        shotshound.Play();
+    }
+
+    [ClientRpc]
+    public void RespawnClientRpc() //sets the gameobject (player component) active for all players
+    {
+        gameObject.SetActive(true);
+    }
+
+    [ClientRpc]
+    public void DespawnClientRpc() //sets the main cam to active and unlocks the cursor, sets the player to deactive for every player
+    {
+        if (IsOwner)
+        {
+            mainCam.SetActive(true);
+            Screen.lockCursor = false;
+        }
+        gameObject.SetActive(false);
+    }
+    #endregion
+
+    //All the messages from clients to the server
+    #region ServerRPC
+    [ServerRpc]
+    public void RespawnServerRpc(int value) //respawns the client for the server and resets the health for the player
+    {
+        RespawnClientRpc();
+
+        Debug.Log("Setting health to 100 for player: " + OwnerClientId);
+        PlayerNetwork player = gameObject.GetComponent<PlayerNetwork>();
+        player.health.Value += value;
     }
 
     [ServerRpc]
-    public void ShootBulletServerRpc(int damage, Vector3 pos, Vector3 dir)
+    public void ShootBulletServerRpc(int damage, Vector3 pos, Vector3 dir) //shoots a bullet (raycast) (via server) and the player that gets hit gets the damage
     {
-        print("Raycast send");
         ShotClientRpc();
         if (Physics.Raycast(pos, dir, out RaycastHit hit) && hit.transform.TryGetComponent(out PlayerNetwork player))
         {
-            print("Damage done");
             player.health.Value -= damage;
             player.hit = true;
 
@@ -156,43 +190,5 @@ public class PlayerNetwork : NetworkBehaviour
             }
         }
     }
-
-    [ClientRpc]
-    public void BloodVFXClientRpc()
-    {
-        blood.Play();
-    }
-
-    [ClientRpc]
-    public void ShotClientRpc()
-    {
-        gunshot.Play();
-        gunAudio.Play();
-    }
-
-    [ClientRpc]
-    public void RespawnClientRpc()
-    {
-        gameObject.SetActive(true);
-    }
-
-    [ServerRpc]
-    public void respawnServerRpc(int value)
-    {
-        RespawnClientRpc();
-        Debug.Log("Setting health to 100");
-        PlayerNetwork player = gameObject.GetComponent<PlayerNetwork>();
-        player.health.Value += value;
-    }
-
-    [ClientRpc]
-    public void despawnclientrpc()
-    {
-        if (IsOwner)
-        {
-            maincam.SetActive(true);
-            Screen.lockCursor = false;
-        }
-        gameObject.SetActive(false);
-    }
+    #endregion
 }
